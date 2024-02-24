0f5fb5ca2dd5eb69767279d42b78b5b268a3a1b3
Victor Soto <vicsonic@gmail.com>
2024-02-24 16:28:10 -0600

    Set initial project structure

---
+++ WeatherApp/Modules/Features/Weather/WeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Victor Soto on 24/02/24.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
